6953ee6516c7a1bf47d78e4d38c4891bf02453b9
Skaglietty <skaglietty@rambler.ru>
2024-09-20 15:49:58 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://2be81075-927c-4c6f-b44d-321e38973d79.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
